e2296e39f0593b8139f51f6633261af060a0aa1b
Tarun <tarunsinh.chaudhari@nl.abnamro.com>
2021-04-20 03:27:17 +0200

    Framework added with basic functions  : Only for simulator.

---
+++ Advertisement.framework/Headers/Advertisement.h
@@ -0,0 +1,18 @@
+//
+//  Advertisement.h
+//  Advertisement
+//
+//  Created by Tarun on 19/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Advertisement.
+FOUNDATION_EXPORT double AdvertisementVersionNumber;
+
+//! Project version string for Advertisement.
+FOUNDATION_EXPORT const unsigned char AdvertisementVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Advertisement/PublicHeader.h>
+
+
